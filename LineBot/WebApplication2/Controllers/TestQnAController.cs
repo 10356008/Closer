d07021a2c637759dc0201e54284b952ec31b6d20
closer <10356008@ntub.edu.tw>
2018-10-18 12:58:32 +0800

    Update LineBot-second

--- LineBot/WebApplication2/Controllers/TestQnAController.cs
+++ LineBot/WebApplication2/Controllers/TestQnAController.cs
@@ -12,7 +12,7 @@ namespace StudyHostExampleLinebot.Controllers
         const string channelAccessToken = "BXFOUD+4p4GuBzEly7y4O2khG8qmxaBttbQwV+4qjYcOTAVIO4nKM+MvQCt8YbYFuY3G/vdPeKhW55wLX4ucuTAjlZwHTyhf9PfCZDbmXW8R1FLYhMBARftvIZ/ao3mInNlusj4zwtvSCAvNcJdsIAdB04t89/1O/w1cDnyilFU=";
         const string AdminUserId = "U04fcc2c004642a37234aa6767448b58c";
         const string QnAKey = "ca1a0dd2-c0aa-40b3-a810-168533df7efe";
-        const string Endpoint = "https://closertest.azurewebsites.net/qnamaker/knowledgebases/3e70ac97-c64a-4881-9811-83dfa3bc1615/generateAnswer"; 
+        const string Endpoint = "https://closertest.azurewebsites.net/qnamaker/knowledgebases/3e70ac97-c64a-4881-9811-83dfa3bc1615/generateAnswer";
         const string UnknowAnswer = "不好意思，您可以換個方式問嗎? 我不太明白您的意思...";
 
         [Route("api/TestQnA")]
@@ -30,27 +30,69 @@ namespace StudyHostExampleLinebot.Controllers
                 //回覆訊息
                 if (LineEvent.type == "message")
                 {
-                    var repmsg = "";
+                    var reply = "";
+                    reply = LineEvent.message.text;
+
                     if (LineEvent.message.type == "text") //收到文字
-                    {  
-                        if(LineEvent.message.text == "使用說明")
+                    {
+                        if(reply == "嗨" || reply == "你好" || reply == "妳好" || reply == "哈囉" || reply == "哈摟" || reply == "哈嘍" || reply == "Hello" || reply == "Hi" || reply == "HELLO" || reply == "HI" || reply == "hello" || reply == "hi")
                         {
-                            repmsg = "";
-                            repmsg = $"您可以問我們關於社交上的問題!!!";
-                            this.ReplyMessage(LineEvent.replyToken, repmsg);
-                        }else if(LineEvent.message.text == "關於我們")
-                        {
-                            repmsg = "";
-                            repmsg = $"NTUB IMD";
-                            this.ReplyMessage(LineEvent.replyToken, repmsg);
+                            //建立actions，作為ButtonTemplate的用戶回覆行為
+                            var actions = new List<isRock.LineBot.TemplateActionBase>();
+                            actions.Add(new isRock.LineBot.PostbackActon() { label = "開始聊天", data = "有什麼社交上的困擾嗎?" });
+                            actions.Add(new isRock.LineBot.UriActon() { label = "瀏覽學習頁面", uri = new Uri("http://140.131.114.147/detailsView") });
+
+
+                            var ButtonTemplateMsg = new isRock.LineBot.ButtonsTemplate()
+                            {
+                                title = "嗨!",
+                                text = "請問您要:",
+                                altText = "嗨 ButtonTemplate",
+                                actions = actions
+                            };
+                            this.PushMessage(LineEvent.source.userId, ButtonTemplateMsg);
                         }
-                        else if (LineEvent.message.text == "學習頁面")
+                        else if (reply == "使用說明")
+                        {
+                            //建立actions，作為ButtonTemplate的用戶回覆行為
+                            var actions = new List<isRock.LineBot.TemplateActionBase>();
+                            actions.Add(new isRock.LineBot.PostbackActon() { label = "開始聊天", data = "有什麼社交上的困擾嗎?" });
+                            actions.Add(new isRock.LineBot.UriActon() { label = "瀏覽學習頁面", uri = new Uri("http://140.131.114.147/detailsView") });
+
+
+                            var ButtonTemplateMsg = new isRock.LineBot.ButtonsTemplate()
+                            {
+                                title = "使用說明",
+                                text = "我們可以針對您在校園社交上的問題進行解惑(ex:要和新同學聊什麼?)，或是可以瀏覽我們的學習頁面網站。",
+                                altText = "使用說明 ButtonTemplate",
+                                actions = actions
+                            };
+                            this.PushMessage(LineEvent.source.userId, ButtonTemplateMsg);
+                        }
+                        else if (reply == "關於我們")
+                        {
+
+                            //建立actions，作為ButtonTemplate的用戶回覆行為
+                            var actions = new List<isRock.LineBot.TemplateActionBase>();
+                            actions.Add(new isRock.LineBot.UriActon() { label = "查看更多", uri = new Uri("http://140.131.114.147/about") });
+
+                            var ButtonTemplateMsg = new isRock.LineBot.ButtonsTemplate()
+                            {
+                                title = "關於我們",
+                                text = "您好，我們是國立臺北商業大學 資訊管理系的學生。",
+                                altText = "使用說明 ButtonTemplate",
+                                actions = actions
+                            };
+                            this.PushMessage(LineEvent.source.userId, ButtonTemplateMsg);
+
+                        }
+                        else if (reply == "學習頁面")
                         {
                             var flex = @"
                                 [
                                 {
                                     ""type"": ""flex"",
-                                    ""altText"": ""This is a Flex Message"",
+                                    ""altText"": ""Closer | Learn Web"",
                                     ""contents"":
                                 {
                                   ""type"": ""bubble"",
@@ -72,11 +114,7 @@ namespace StudyHostExampleLinebot.Controllers
                                     ""url"": ""https://imageshack.com/a/img924/8986/kuGMSb.png"",
                                     ""size"": ""full"",
                                     ""aspectRatio"": ""20:13"",
-                                    ""aspectMode"": ""cover"",
-                                    ""action"": {
-                                      ""type"": ""uri"",
-                                      ""uri"": ""http://140.131.114.147:3000/""
-                                    }
+                                    ""aspectMode"": ""cover""
                                   },
                                   ""body"": {
                                     ""type"": ""box"",
@@ -161,7 +199,7 @@ namespace StudyHostExampleLinebot.Controllers
                                         ""action"": {
                                           ""type"": ""uri"",
                                           ""label"": ""更多內容"",
-                                          ""uri"": ""http://140.131.114.147:3000/""
+                                          ""uri"": ""http://140.131.114.147""
                                         }
                                       }
                                     ]
@@ -171,14 +209,15 @@ namespace StudyHostExampleLinebot.Controllers
                                 ]
                                 ";
 
-                            this.PushMessagesWithJSON(AdminUserId,flex);
+
+                            this.PushMessagesWithJSON(LineEvent.source.userId, flex);
                         }
                         else
                         {
                             //建立 MsQnAMaker Client
                             var helper = new isRock.MsQnAMaker.Client(
                             new Uri(Endpoint), QnAKey);
-                            var QnAResponse = helper.GetResponse(LineEvent.message.text.Trim());
+                            var QnAResponse = helper.GetResponse(reply.Trim());
                             var ret = (from c in QnAResponse.answers
                                        orderby c.score descending
                                        select c
@@ -189,10 +228,64 @@ namespace StudyHostExampleLinebot.Controllers
                                 responseText = ret.FirstOrDefault().answer;
                             //回覆
                             this.ReplyMessage(LineEvent.replyToken, responseText);
-                        }          
+
+                            //建立actions，作為ButtonTemplate的用戶回覆行為
+                            var actions = new List<isRock.LineBot.TemplateActionBase>();
+                            actions.Add(new isRock.LineBot.PostbackActon() { label = "繼續聊天", data = "還想問些什麼呢?" });
+                            actions.Add(new isRock.LineBot.PostbackActon() { label = "結束聊天", data = "結束" });
+
+
+                            var ButtonTemplateMsg = new isRock.LineBot.ButtonsTemplate()
+                            {
+                                title = "還想問些什麼嗎?",
+                                text = "選擇:",
+                                altText = "ButtonTemplate",
+                                actions = actions
+                            };
+                            this.PushMessage(LineEvent.source.userId, ButtonTemplateMsg);
+
+                        }
                     }
                     if (LineEvent.message.type == "sticker") //收到貼圖
                         this.ReplyMessage(LineEvent.replyToken, 1, 2);
+                }
+                if (LineEvent.type == "postback")
+                {
+                    var data = LineEvent.postback.data;
+                    var repmsg = "";
+
+                    if(data == "結束")
+                    {
+                        //建立actions，作為ButtonTemplate的用戶回覆行為
+                        var actions = new List<isRock.LineBot.TemplateActionBase>();
+                        actions.Add(new isRock.LineBot.PostbackActon() { label = "有", data = "有幫助" });
+                        actions.Add(new isRock.LineBot.PostbackActon() { label = "沒有", data = "沒有幫助" });
+
+
+                        var ButtonTemplateMsg = new isRock.LineBot.ButtonsTemplate()
+                        {
+                            title = "感謝您本次的提問!",
+                            text = "請問我們的回答對您有幫助嗎?",
+                            altText = "結束聊天 ButtonTemplate",
+                            actions = actions
+                        };
+                        this.PushMessage(LineEvent.source.userId, ButtonTemplateMsg);
+                    }
+                    else if(data == "有幫助")
+                    {
+                        repmsg = "感謝您本次的提問！您的回饋使我們更加進步。";
+                        this.ReplyMessage(LineEvent.replyToken, repmsg);
+                    }
+                    else if (data == "沒有幫助")
+                    {
+                        repmsg = "感謝您本次的提問！我們會針對這次的對談進行修正。";
+                        this.ReplyMessage(LineEvent.replyToken, repmsg);
+                    }
+                    else
+                    {
+                        this.ReplyMessage(LineEvent.replyToken, data);
+                    }
+
                 }
                 //response OK
                 return Ok();
@@ -206,5 +299,7 @@ namespace StudyHostExampleLinebot.Controllers
             }
 
         }
+
+
     }
 }
